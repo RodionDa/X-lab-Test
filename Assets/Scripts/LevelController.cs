bc58fbef868a7191b25fdddfcd970c27567a59b3
Rodion <r.dausht@xlab-game.com>
2024-11-10 20:39:57 +0300

    ALFA 1.3

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -11,8 +11,10 @@ namespace Golf
         public Stick stick;
         public StoneSpawner stoneSpawner;
         private float m_timer;
+
         [SerializeField]
-        private float m_delay = 2f;
+        private float m_initialDelay = 2f; // Начальная задержка (в секундах)
+        private float m_delay; // Текущая задержка
         private int m_score = 0;
 
         private List<Stone> m_stones = new List<Stone>();
@@ -20,13 +22,13 @@ namespace Golf
         public event Action<int> onGameOver;
         public event Action<int> onScoreInc;
 
-        public void OnEnable()
+        private void OnEnable()
         {
+            m_delay = m_initialDelay; // Инициализируем текущую задержку как начальную
             m_timer = Time.time - m_delay;
             stick.onCollisionStone += OnCollisionStick;
 
             m_score = 0;
-
             ClearStones();
         }
 
@@ -50,23 +52,29 @@ namespace Golf
 
         private void Update()
         {
+            // Проверка времени, чтобы спавнить камни с учётом текущей задержки
             if (Time.time > m_timer + m_delay)
             {
                 m_timer = Time.time;
 
+                // Спавним камень
                 var go = stoneSpawner.Spawn();
                 var stone = go.GetComponent<Stone>();
 
+                // Подписываемся на событие столкновения с камнем
                 stone.onCollisionStone += OnCollisionStone;
 
+                // Добавляем камень в список
                 m_stones.Add(stone);
-            }
 
+                // Проверяем, нужно ли уменьшить задержку
+                AdjustSpawnSpeed();
+            }
         }
 
         private void OnCollisionStick()
         {
-            m_score++; 
+            m_score++;
             Debug.Log($"score: {m_score}");
             onScoreInc?.Invoke(m_score);
         }
@@ -77,9 +85,32 @@ namespace Golf
             onGameOver?.Invoke(m_score);
         }
 
-         private void OnTriggerEnter(Collider other)
+        // Метод для увеличения скорости спавна
+        private void AdjustSpawnSpeed()
         {
-            
+            // Уменьшаем задержку только после достижения 10, 20, 30 и т.д. очков
+            if (m_score >= 10 && m_score % 10 == 0)
+            {
+                // Уменьшаем задержку в 2 раза (ускоряем спавн)
+                float oldDelay = m_delay; // Сохраняем старую задержку
+
+                // Уменьшаем задержку в два раза
+                m_delay /= 2f;
+
+                // Ограничиваем минимальную задержку (например, 0.5 секунд)
+                m_delay = Mathf.Max(m_delay, 0.5f);
+
+                // Выводим информацию о старой и новой задержке
+                if (oldDelay != m_delay) // Выводим только если задержка изменилась
+                {
+                    Debug.Log($"Spawn speed increased! Old delay: {oldDelay:F2}s, New delay: {m_delay:F2}s");
+                }
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            // Ваш код для обработки триггеров (если нужно)
         }
     }
 }
--- Assets/Scripts/TriggerController.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using Golf;
-using UnityEngine;
-
-public class TriggerController : MonoBehaviour
-{
-  
-}
